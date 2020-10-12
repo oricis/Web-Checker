fa949facea26afef49ef54b4cf8d9bd8fd757cb5
moises.alcocer <contacto@ironwoods.es>
2020-10-12 13:32:01 +0200

    added HTML5 & CSS validations

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,8 +1,23 @@
 const runCheckers = (url) => {
-    console.log('Target: ' + url);
+    console.log('Target: ' + url); // HACK: trace
 
-    let CssValitador   = 'https://jigsaw.w3.org/css-validator/validator?uri=';
-    let Html5Valitador = 'https://html5.validator.nu/?doc=';
+
+    const CssValitador   = 'https://jigsaw.w3.org/css-validator/validator?uri=' + url;
+    const Html5Valitador = 'https://html5.validator.nu/?doc=' + url;
+
+    const arrLinks = [
+        CssValitador,
+        Html5Valitador,
+    ];
+
+
+    for (let i = 0; i < arrLinks.length; i++) {
+        openNewTab(arrLinks[i]);
+    }
+}
+
+const openNewTab = (url) => {
+    window.open(url, '_blank');
 }
 
 
