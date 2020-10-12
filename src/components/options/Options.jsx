25f57aa6f2c9a649fdf4962a14c0ba4fee23558b
moises.alcocer <contacto@ironwoods.es>
2020-10-13 00:41:37 +0200

    checked options with option groups

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Checkbox from '../forms/Checkbox.jsx';
 import SmallButton from '../buttons/SmallButton.jsx';
+import { arrAllOptions, arrBasicOptions } from '../../data/group-options.js';
 import './Options.css';
 
 
@@ -11,27 +12,26 @@ const handleCheckOption = (id, checked) => {
 function Options()
 {
     const [optionsGroupName, setOptionGroupSelected] = useState('basic');
+    const [optionsSelected, setOptionsSelected] = useState(arrBasicOptions);
+    console.log('options: ' + optionsGroupName); // HACK: trace
 
     const selectAll = () => {
         console.log('selectAll');
         const groupName = 'all';
         setOptionGroupSelected(groupName);
-
-        // TODO: select all the options
+        setOptionsSelected(arrAllOptions);
     }
     const selectBasic = () => {
         console.log('selectBasic');
         const groupName = 'basic';
         setOptionGroupSelected(groupName);
-
-        // TODO: select options in the group
+        setOptionsSelected(arrBasicOptions);
     }
     const selectNone = () => {
         console.log('selectNone');
         const groupName = 'none';
         setOptionGroupSelected(groupName);
-
-        // TODO: deselect all the options
+        setOptionsSelected([]);
     }
 
     const getCssExtraClass = (expectedOption, actualButton) => {
@@ -42,7 +42,7 @@ function Options()
     const cssClassNameForNone  = getCssExtraClass('none', optionsGroupName);
 
     return (
-        <section class="options">
+        <section className="options">
             <h3>Options:</h3>
 
             <div className="option-buttons">
@@ -60,91 +60,91 @@ function Options()
 
             <div className="checkboxes">
                 <Checkbox
-                    checked={true}
+                    checked={optionsSelected.includes('css')}
                     defaultValue="css"
                     id="css"
                     text="CSS"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('facebook')}
                     defaultValue="facebook"
                     id="facebook"
                     text="Debug Facebook Metas"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('gtmetrix')}
                     defaultValue="gtmetrix"
                     id="gtmetrix"
                     text="GtMextrix Report"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={true}
+                    checked={optionsSelected.includes('html5')}
                     defaultValue="html5"
                     id="html5"
                     text="HTML5"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('js')}
                     defaultValue="js"
                     id="js"
                     text="JavaScript Validation"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={true}
+                    checked={optionsSelected.includes('links')}
                     defaultValue="links"
                     id="links"
                     text="Links"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('microdata')}
                     defaultValue="microdata"
                     id="microdata"
                     text="Microdata"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={true}
+                    checked={optionsSelected.includes('mobile-friendly')}
                     defaultValue="mobile-friendly"
                     id="mobile-friendly"
                     text="Mobile Friendly"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('mozilla')}
                     defaultValue="mozilla"
                     id="mozilla"
                     text="Mozilla Observatory Report"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('page-speed')}
                     defaultValue="page-speed"
                     id="page-speed"
                     text="PageSpeed Report"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('headers')}
                     defaultValue="headers"
                     id="headers"
                     text="Security Headers"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('site-cost')}
                     defaultValue="site-cost"
                     id="site-cost"
                     text="Site Cost Report"
                     onChange={(id, checked) => { handleCheckOption(id, checked) }}>
                 </Checkbox>
                 <Checkbox
-                    checked={false}
+                    checked={optionsSelected.includes('keywords-density')}
                     defaultValue="keywords-density"
                     id="keywords-density"
                     text="SEO - Keywords density"
