6764b831004199e6c53a56e26e2dcdc75ec4b79a
moises.alcocer <contacto@ironwoods.es>
2020-10-13 00:30:54 +0200

    added groups of options

---
+++ src/data/group-options.js
@@ -0,0 +1,28 @@
+const arrAllOptions = [
+    'css',
+    'facebook',
+    'gtmetrix',
+    'headers',
+    'html5',
+    'js',
+    'keywords-density',
+    'links',
+    'microdata',
+    'mobile-friendly',
+    'mozilla',
+    'page-speed',
+    'site-cost',
+];
+
+const arrBasicOptions = [
+    'css',
+    'html5',
+    'links',
+    'mobile-friendly',
+];
+
+
+export {
+    arrAllOptions,
+    arrBasicOptions
+}
