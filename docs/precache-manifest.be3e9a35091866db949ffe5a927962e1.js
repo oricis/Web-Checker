514d62182fc67e8c80391072b5084425ca4327c4
moises.alcocer <contacto@ironwoods.es>
2020-10-16 10:25:52 +0200

    added build files to doc to publish

---
+++ docs/precache-manifest.be3e9a35091866db949ffe5a927962e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626f9b1bab1f8163fe149584aa9f0a3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f37e16f664741196a50b",
+    "url": "/static/css/main.b035f45c.chunk.css"
+  },
+  {
+    "revision": "a26c92fb03ec9ef23309",
+    "url": "/static/js/2.8186246e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8186246e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37e16f664741196a50b",
+    "url": "/static/js/main.9c90795f.chunk.js"
+  },
+  {
+    "revision": "f98cb6e5f81d57576da5",
+    "url": "/static/js/runtime-main.18d393e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
